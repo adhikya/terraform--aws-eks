361cd209f405d80d1edd694d7be1a07d7eaf8206
adhikya <ch.adhikya@gmail.com>
2025-09-05 16:24:55 +0800

    jenkins

--- 60-acm/variables.tf
+++ 60-acm/variables.tf
@@ -7,9 +7,9 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z032558618100M4EJX8X4"
+    default = "Z071190310E2CDMEOGGMZ"
 }
 
 variable "zone_name" {
-    default = "daws84s.site"
+    default = "adhikya.site"
 }
